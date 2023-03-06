7d026eac88f1c0942e90ce249792673a7a2bd8a5
giovannitamburini <giovatambu@gmail.com>
2023-03-06 12:29:25 +0100

    added list random numbers , added bomb

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/*
+CONSEGNA
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+*/
+
+
 
 //- creo un bottone nel body HTML e lo richiamo in js
 let generatorElement = document.getElementById('generator');
@@ -16,6 +29,15 @@ let resetElement = document.getElementById('reset');
 
 let numberCells = 0;
 
+
+// ESERCIZIO CAMPOMINATO DOM
+
+// - creo una variabile per generare tramite funzione un numero casuale
+let randomNumber;
+
+let bombNumbers = [];
+
+
 // - creo un evento click sul bottone
 generatorElement.addEventListener('click', function() {
 
@@ -27,6 +49,7 @@ generatorElement.addEventListener('click', function() {
 
     // creo una varibaile per impostare la misura della cella in px in base alla difficoltà selezionata
     let cellPixel;
+
 
     // - condizione difficoltà selezionata = easy
     if (difficultyElement.value == 'easy') {
@@ -58,11 +81,15 @@ generatorElement.addEventListener('click', function() {
     while (contatore < numberCells) {
 
         // - richiamo la funzione generatrice di cella
-        createCell(cellContainerElement, contatore + 1, cellPixel);
+        createCell(cellContainerElement, contatore + 1, cellPixel, bombNumbers);
 
         // - aggiungo un unità al contatore per non creare un loop infinito
         contatore++
     }
+
+
+    // - implemento la funzione generatrice di numeri casuali
+    generateNumbers(numberCells, bombNumbers);
 
     // tolgo i tasti di selezione difficoltà e del generatore di griglia
     optionContainerElement.style.display = 'none';
@@ -93,6 +120,9 @@ resetElement.addEventListener('click', function(){
 
     // rendo di nuovo disponibile il tasti di selezione difficoltà e di generazione griglia
     optionContainerElement.style.display = 'flex';
+
+    //svuoto la lista di numeri creati casualmente
+    bombNumbers = [];
 })
 
 
@@ -100,7 +130,7 @@ resetElement.addEventListener('click', function(){
 //FUNCTION----------------------------------------------
 
 //- funzione generatrice di cella
-function createCell (container, index, pixel) {
+function createCell (container, index, pixel, array) {
 
     //creo un elemento div
     let cell = document.createElement('div');
@@ -124,11 +154,22 @@ function createCell (container, index, pixel) {
     //aggiungo un evento click al parametro cella
     cell.addEventListener('click', function() {
 
-        //aggiungo/cambio il background dell'elemento cell
-        cell.style.backgroundColor = '#00FFFF';
-
         //stampo in console il parametro index inserito all'interno dell'elemento cell
         console.log(index);
+
+        // condizione di verifica della presenza di un numero nella lista array dove sono indicati i numeri in cui sono inserite le bombe
+        if (array.includes(index)) {
+
+            cell.style.backgroundColor = 'red';
+
+            cell.removeEventListener('click', function(){});
+
+        } else {
+
+            //aggiungo/cambio il background dell'elemento cell
+            cell.style.backgroundColor = '#00FFFF';
+
+        }
     })
 
     return cell
@@ -136,9 +177,29 @@ function createCell (container, index, pixel) {
 }
 
 
-//funzione generatrice numero casuale
+//funzione generatrice numero casuale dove il parametro max indica il massimo numero creabile
+function generateNumbers (max, array) {
 
-function randomNumber () {
+    // creo un contatore per uscire dal ciclo
+    contatoreRandom = 0;
 
-    
+    // creo un ciclo per generare numeri casuali
+    while (contatoreRandom < 16) {
+
+        let randomNumber = Math.floor(Math.random() * (max - 1 + 1) + 1);
+
+        if(array.includes(randomNumber)) {
+
+            contatoreRandom--
+            
+        } else {
+            
+            array.push(randomNumber);
+
+        }
+        
+        contatoreRandom++
+    }
+
+    return randomNumber;
 }
